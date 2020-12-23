5de45792c8147090b9da4875e0ef97e8de4a1227
moshe vilner <shootermv@gmail.com>
2020-12-23 13:53:01 +0200

    💯pagination component added

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Pagination.css";
+
+/* 
+pagination component has exaclty 9 pages -
+this is something i did because
+it looked this way in the scetches and i had limited time...
+*/
+
+export const Pagination = ({currentPage, onPgClicked}) => {
+  return (
+    <ul className="pagination">
+        <li className="pagination--forward" disabled={+currentPage === 1} onClick={() => {
+          if (currentPage > 1)
+            onPgClicked(currentPage - 1)
+          }}>&lt;</li>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(pg => {
+           return <li onClick={() => onPgClicked(pg)} className={`${+currentPage === pg ? 'current' : ''}`}>{pg}</li>
+        })}
+        <li className="pagination--backward" disabled={+currentPage === 9}  onClick={() => {
+          if (currentPage < 9)
+            onPgClicked(currentPage + 1)
+          }}>&gt;</li>
+    </ul>
+  );
+};
