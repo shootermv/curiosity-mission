193634881c2943ea8993c7983c7b96c42ed45d7e
moshe vilner <shootermv@gmail.com>
2020-12-23 16:05:11 +0200

    change browserrouter to Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import AboutPage from "./Pages/About/AboutPage";
 import WeatherPage from "./Pages/Weather/WeatherPage";
 import ImagesPage from "./Pages/Images/ImagesPage";
