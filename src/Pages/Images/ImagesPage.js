c341720dd5928571ae6a34d311eaf1573da155cb
moshe vilner <shootermv@gmail.com>
2020-12-23 12:40:44 +0200

    adding use-http package 🔥

--- src/Pages/Images/ImagesPage.js
+++ src/Pages/Images/ImagesPage.js
@@ -1,5 +1,14 @@
 import React from "react";
+import useFetch from "use-http";
 
 export default function ImagesPage() {
-  return <section>Images Page here</section>;
+  const url = process.env.REACT_APP_NASA_PHOTO_URL;
+  const key = process.env.REACT_APP_NASA_KEY;
+  let page = 1;
+  const { data, loading, error } = useFetch(
+    `${url}?earth_date=2020-07-14&api_key=${key}&page=${page}`,
+    {},
+    []
+  );
+  return <section>Images Page here {data?.photos.length}</section>;
 }
--- src/Pages/Weather/WeatherPage.js
+++ src/Pages/Weather/WeatherPage.js
@@ -1,5 +1,13 @@
 import React from "react";
-
+import useFetch from "use-http";
 export default function WeatherPage() {
+  const url = process.env.REACT_APP_NASA_WEATHER_URL;
+  const key = process.env.REACT_APP_NASA_KEY;
+  let page = 1;
+  const { data, loading, error } = useFetch(
+    `${url}/?api_key=${key}&feedtype=json&ver=1.0&page=${page}`,
+    {},
+    []
+  );
   return <section>Weather Page here</section>;
 }
